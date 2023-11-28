c4a3f347ca36ed79b40b79b185b322120fc9877a
AlessandroSellside <Alessandro.Diaz@sellside.cl>
2023-11-28 09:14:45 -0600

    link ersonalizado de facebook

--- src/components/VerticalButtons.jsx
+++ src/components/VerticalButtons.jsx
@@ -14,7 +14,7 @@ const VerticalButtons = () => {
     <>
          <div className="flex justify-center gap-10 mb-4">
             <div className='flex-col'data-aos="fade-right" >
-            <Link to='https://www.facebook.com/profile.php?id=61554017310537' target="_blank"><button className="flex items-center w-32 mb-2 transition duration-500 bg-gray-600 rounded-lg lg:h-16 lg:w-44 hover:scale-110 lg:mt-5 md:h-12 md:w-40 md:mb-4 sm:w-40 sm:mb-5" >
+            <Link to='https://www.facebook.com/ZarpeCateringService' target="_blank"><button className="flex items-center w-32 mb-2 transition duration-500 bg-gray-600 rounded-lg lg:h-16 lg:w-44 hover:scale-110 lg:mt-5 md:h-12 md:w-40 md:mb-4 sm:w-40 sm:mb-5" >
                   <img className='w-10 h-auto' src={facebokLogo} alt="" />
                   <p className='ml-2 text-white lg:text-xl'>Facebook</p>
               </button></Link>
